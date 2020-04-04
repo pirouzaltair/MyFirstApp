b408587a9eb1dca7f0600f067e58b3e6bbb50042
Pirouz Mehmandoost <pirouzm@Alairs-MacBook-Pro.local>
2020-04-04 14:20:21 -0700

    First commit.

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Pirouz Mehmandoost on 4/3/20.
+//  Copyright © 2020 Pirouz Mehmandoost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
